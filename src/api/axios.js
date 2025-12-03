39120d072d0fc609a85c8853f0a98cf17e384e42
Aaronleon13 <aaronleon13@gmail.com>
2025-12-03 09:34:58 -0700

    feat: Add Axios response interceptor for automatic token refresh and login redirection on 401 errors.

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,45 @@
 import axios from 'axios'
+import router from '../router'
 
-const axiosInstance = axios.create({
+const api = axios.create({
   baseURL: 'https://api-curso-production.up.railway.app',
   withCredentials: true,
 })
 
-export default axiosInstance
+api.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    const originalRequest = err.config
+
+    const authEndPoints = ['auth/login', 'auth/register']
+
+    const isAuthEndPoint = authEndPoints.some((endPoint) => originalRequest.url.includes(endPoint))
+
+    if (isAuthEndPoint) {
+      return Promise.reject(err)
+    }
+
+    if (err.response.status === 401 && originalRequest._retry) {
+      localStorage.removeItem('token')
+      router.push('/login')
+      return Promise.reject(err)
+    }
+
+    if (err.response.status === 401) {
+      originalRequest._retry = true
+
+      try {
+        await api.post('auth/refresh-token')
+        return api(originalRequest)
+      } catch (error) {
+        localStorage.removeItem('token')
+        router.push('/login')
+        return Promise.reject(error)
+      }
+    }
+
+    return Promise.reject(err)
+  },
+)
+
+export default api
